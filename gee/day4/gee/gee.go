bde0d27442923aefb6870c90faaa8f32967e7344
jack <jjy_8888@163.com>
2023-04-08 19:05:41 +0800

    add  group function

---
+++ gee/day4/gee/gee.go
@@ -0,0 +1,65 @@
+package gee
+
+import (
+	"log"
+	"net/http"
+)
+
+type HandlerFunc func(*Context)
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	parent      *RouterGroup
+	engine      *Engine
+}
+
+type Engine struct {
+	router *router
+	*RouterGroup
+	groups []*RouterGroup
+}
+
+func New() *Engine {
+	e := &Engine{
+		router: newRouter(),
+	}
+	e.RouterGroup = &RouterGroup{engine: e}
+	e.groups = []*RouterGroup{e.RouterGroup}
+	return e
+}
+
+func (g *RouterGroup) Group(prefix string) *RouterGroup {
+	e := g.engine
+	newGroup := &RouterGroup{
+		prefix: g.prefix + prefix,
+		parent: g,
+		engine: e,
+	}
+	e.groups = append(e.groups, newGroup)
+	return newGroup
+}
+
+func (g *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := g.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	g.engine.router.addRoute(method, pattern, handler)
+}
+
+func (g *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	g.addRoute("GET", pattern, handler)
+}
+
+func (g *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	g.addRoute("POST", pattern, handler)
+}
+
+func (e *Engine) Run(addr string) (err error) {
+	log.Printf("server listen on %s\n", addr)
+	return http.ListenAndServe(addr, e)
+}
+
+func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	e.router.handle(c)
+}
