f0d71377cf75fc940be65c9d53ff7865b080c2a9
jack <jjy_8888@163.com>
2023-04-09 09:49:12 +0800

    add middleware support

---
+++ gee/day5/gee/gee.go
@@ -0,0 +1,77 @@
+package gee
+
+import (
+	"log"
+	"net/http"
+	"strings"
+)
+
+type HandlerFunc func(*Context)
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	parent      *RouterGroup
+	engine      *Engine
+}
+
+type Engine struct {
+	router *router
+	*RouterGroup
+	groups []*RouterGroup
+}
+
+func New() *Engine {
+	e := &Engine{
+		router: newRouter(),
+	}
+	e.RouterGroup = &RouterGroup{engine: e}
+	e.groups = []*RouterGroup{e.RouterGroup}
+	return e
+}
+
+func (g *RouterGroup) Group(prefix string) *RouterGroup {
+	e := g.engine
+	newGroup := &RouterGroup{
+		prefix: g.prefix + prefix,
+		parent: g,
+		engine: e,
+	}
+	e.groups = append(e.groups, newGroup)
+	return newGroup
+}
+
+func (g *RouterGroup) Use(middlewares ...HandlerFunc) {
+	g.middlewares = append(g.middlewares, middlewares...)
+}
+
+func (g *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := g.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	g.engine.router.addRoute(method, pattern, handler)
+}
+
+func (g *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	g.addRoute("GET", pattern, handler)
+}
+
+func (g *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	g.addRoute("POST", pattern, handler)
+}
+
+func (e *Engine) Run(addr string) (err error) {
+	log.Printf("server listen on %s\n", addr)
+	return http.ListenAndServe(addr, e)
+}
+
+func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	var middlewares []HandlerFunc
+	for _, group := range e.groups {
+		if strings.HasPrefix(req.URL.Path, group.prefix){
+			middlewares = append(middlewares, group.middlewares...)
+		}
+	}
+	c := newContext(w, req)
+	c.handlers = middlewares
+	e.router.handle(c)
+}
---
+++ gee/day5/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"example/gee"
+	"log"
+	"net/http"
+	"time"
+)
+
+func onlyForV2() gee.HandlerFunc {
+	return func(ctx *gee.Context) {
+		t := time.Now()
+		ctx.Next()
+		log.Printf("v2 logger [%d] %s in %v", ctx.StatusCode, ctx.Req.RequestURI, time.Since(t))
+	}
+}
+
+func main() {
+	r := gee.New()
+	r.Use(gee.Logger())
+	r.GET("/index", func(c *gee.Context) {
+		c.Html(http.StatusOK, "<h1>Index Page</h1>")
+	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gee.Context) {
+			c.Html(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
+
+		v1.GET("/hello", func(c *gee.Context) {
+			// expect /hello?name=geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	v2.Use(onlyForV2())
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
+
+	r.Run(":9999")
+}
