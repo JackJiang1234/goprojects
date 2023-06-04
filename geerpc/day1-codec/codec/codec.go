5fb395deeb137c047fd1d1c1448d630b9bc13ea7
jack <jjy_8888@163.com>
2023-06-04 17:27:49 +0800

    add gee rpc day1

---
+++ geerpc/day1-codec/codec/codec.go
@@ -0,0 +1,34 @@
+package codec
+
+import (
+	"io"
+)
+
+type Header struct {
+	ServiceMethod string
+	Seq           uint64
+	Error         error
+}
+
+type Codec interface {
+	io.Closer
+	ReadHeader(*Header) error
+	ReadBody(any) error
+	Write(*Header, any) error
+}
+
+type NewCodecFunc func (io.ReadWriteCloser) Codec 
+
+type Type string
+
+const (
+	GobType Type = "application/gob"
+	JsonType Type = "application/json"
+)
+
+var NewCodecFuncMap map[Type]NewCodecFunc
+
+func init() {
+	NewCodecFuncMap = make(map[Type]NewCodecFunc, 0)
+	NewCodecFuncMap[GobType] = NewGobCodec
+}
